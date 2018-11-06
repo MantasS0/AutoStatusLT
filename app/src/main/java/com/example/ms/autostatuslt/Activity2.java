0d30e97350b505c5ae5058c3bcb585e245b98f56
MantasS0 <man_tas_pats@yahoo.com>
2018-11-06 17:35:57 +0200

    Main layout tvarkomas. Dar reik daug padaryt...

---
+++ app/src/main/java/com/example/ms/autostatuslt/Activity2.java
@@ -0,0 +1,18 @@
+package com.example.ms.autostatuslt;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class Activity2 extends AppCompatActivity {
+    private static final String TAG = "Activity2";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_layout);
+
+        Log.d(TAG, "onCreate: Started.");
+    }
+}
